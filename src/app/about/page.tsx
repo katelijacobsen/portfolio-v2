979089a813a64a74977c00c401011d59e0d82eb0
katelijacobsen <katjamaehleke98@gmail.com>
2025-10-22 14:41:02 +0200

    rmv

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -83,30 +83,9 @@ const educations = [
   },
 ];
 
-export const containerVariants = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.25, // delay between children
-      delayChildren: 0.3,    // delay before starting stagger
-    },
-  },
-};
-
-export const itemVariants = {
-  hidden: { opacity: 0, y: 40 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: 0.8, ease: "easeOut" },
-  },
-};
-
 export default function Page() {
   return (
     <motion.main
-    
       initial={{ opacity: 0, y: 150 }} // start slightly below and invisible
       animate={{ opacity: 1, y: 0 }} // fade in and slide up
       transition={{ duration: 1.4, ease: "anticipate", delay: 0.1 }}
